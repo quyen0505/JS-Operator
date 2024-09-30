c2269331e29366c2a92175f14a9d6f85a9b5ede1
Nguyen Van Quyen <153894626+quyen0505@users.noreply.github.com>
2024-09-30 13:59:27 +0700

    Add files via upload

---
+++ Operator/js/main.js
@@ -0,0 +1,11 @@
+var a = parseInt(prompt(" Nhap a : "))
+var b = parseInt(prompt(" Nhap b : "))
+var tong = a + b
+var hieu = a - b
+var tich = a * b
+var thuong = a / b
+
+document.write("Tong = " + tong +"<br>")
+document.write("Hieu = " + hieu +"<br>")
+document.write("Tich = " + tich +"<br>")
+document.write("Thuong = " + thuong +"<br>")
